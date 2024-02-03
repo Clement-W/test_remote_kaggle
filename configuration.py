723758894b09d3792a996bb88a1bbd7d512cbcc5
Clément Weinreich <clement@weinreich.fr>
2024-02-03 15:08:57 +0100

    initial commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+NB_ID = "training-notebook"  # This will be the name which appears on Kaggle.
+GIT_USER = "clement-w"  # Your git user name
+GIT_REPO = "test_remote_kaggle"  # Your current git repo
+# Keep free unless you need to acess kaggle datasets. You'll need to modify the remote_training_template.ipynb.
+KAGGLE_DATASET_LIST = []
